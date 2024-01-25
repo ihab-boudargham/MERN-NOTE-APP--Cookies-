57a037fd953419664bca9a1d7f3e3a590446faa1
ihab-boudargham <ihab.boudargham@hotmail.com>
2024-01-26 01:55:42 +0200

    app.use for error handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,22 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import NoteModel from './models/note';
 
 const app = express();
 
-app.get('/', async (req, res) => {
+app.get('/', async (req, res, next) => {
   try {
     const notes = await NoteModel.find().exec();
     console.log('Server Response:', notes);
     res.status(200).json(notes);
   } catch (error) {
-    console.error('Error fetching notes:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    next(error);
   }
 });
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error('Error fetching notes:', error);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
 export default app;
